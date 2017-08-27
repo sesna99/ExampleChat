b3482aa04ec02fa0cd1c8f907ab694ccc838c0b9
sesna99 <sesna99@gmail.com>
2017-08-27 17:08:38 +0900

    1

---
+++ app/src/main/java/com/example/admin/examplechat/ChatListAdapter2.java
@@ -0,0 +1,34 @@
+package com.example.admin.examplechat;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 2017-08-27.
+ */
+
+public class ChatListAdapter2 extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
